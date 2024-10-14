efa3ca24b5cc6b4c313abf82cbb8cc0dd73b8c52
sbhrepo <sbhrepo@users.noreply.github.com>
2024-10-14 09:54:19 -0400

    Update entrypoint.sh
    Add display image environment variables to the index file at start up

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,7 @@
 
 # Replace placeholder in the HTML file with the environment variable
 sed -i "s|{{BACKGROUND_COLOR}}|${BACKGROUND_COLOR}|g" /usr/share/nginx/html/index.html
+sed -i "s|{{DISPLAY_IMAGE}}|${DISPLAY_IMAGE}|g" /usr/share/nginx/html/index.html
 
 # Start nginx
 exec nginx -g 'daemon off;'
