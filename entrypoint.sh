c2860661043700ff71a8fb247c2526b095bfb6d3
sbhrepo <sbhrepo@users.noreply.github.com>
2024-11-12 10:05:51 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Copy the backup index.html file to the nginx directory
+cp /usr/share/nginx/html/backup/index.html /usr/share/nginx/html/index.html
+
 # Replace placeholder in the HTML file with the environment variable
 sed -i "s|{{BACKGROUND_COLOR}}|${BACKGROUND_COLOR}|g" /usr/share/nginx/html/index.html
 sed -i "s|{{DISPLAY_IMAGE}}|${DISPLAY_IMAGE}|g" /usr/share/nginx/html/index.html
