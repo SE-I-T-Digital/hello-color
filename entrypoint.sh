de805775266acd9c6fa9124ea30bf78ce765c76f
sbhrepo <sbhrepo@users.noreply.github.com>
2025-02-12 08:25:20 -0500

    Update entrypoint.sh
    Test for bind mounts

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,7 @@
 
 # Copy the backup index.html file to the nginx directory
 cp /usr/share/nginx/html/backup/index.html /usr/share/nginx/html/index.html
+cp -a /usr/share/nginx/html/files/ /usr/share/nginx/html/
 
 # Replace placeholder in the HTML file with the environment variable
 sed -i "s|{{BACKGROUND_COLOR}}|${BACKGROUND_COLOR}|g" /usr/share/nginx/html/index.html
